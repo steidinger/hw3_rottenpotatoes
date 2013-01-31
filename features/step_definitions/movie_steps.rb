68905a5953c0dacf1e079274bbbf85746ccf6a71
Frank Steidinger <steidinger@acm.org>
2013-01-31 22:58:31 +0100

    fixes Part 1 (movies were not added to DB); implements filter_movie_list.feature scenario 'restrict to R, PG'

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,7 +4,7 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
-    Movie.new(movie)
+    Movie.create(movie)
   end
 end
 
@@ -25,4 +25,8 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_list.split(/, /).each do | rating |
+    step %Q{I check "ratings[#{rating}]"} unless uncheck
+    step %Q{I uncheck "ratings[#{rating}]"} if uncheck
+  end
 end
